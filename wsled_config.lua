5092e6290a4f08d4c00ef7bf40bdf28010c61121
Dirk Braunschweiger <git@dirkmb.de>
2015-08-15 00:02:22 +0200

    new config module

---
+++ wsled_config.lua
@@ -0,0 +1,12 @@
+return {
+    direction = 1,
+    pos = 0,
+    colorR = 0,
+    colorG = 0,
+    colorB = 50,
+    mode = 2,
+    signal_red = 0,
+    signal_red_change = 10,
+    speed = 10,
+    length = 4
+}

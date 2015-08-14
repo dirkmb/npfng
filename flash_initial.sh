e77562312c8aabbf2ad8627dc7d760c23ffd486c
Arne Schauf <git@as-webservices.de>
2015-08-14 23:29:08 +0200

    remove unnecessary stuff

--- flash_initial.sh
+++ flash_initial.sh
@@ -2,24 +2,16 @@
 
 set -e
 
+OLD_PWD=$PWD
 
-for f in 'init', 'pluginloader', 'wificonnect', 'config', 'ledserver', 'wsled'
-do
-    echo "remove $f"
-    echo "file.remove('${f}.lua')" > /dev/ttyUSB0
-    sleep .1
-    echo "file.remove('${f}.lc')" > /dev/ttyUSB0
-    sleep .1
-done
+cd ../npfng/
 
+./flash_initial.sh
 
-./luatool.py -f init.lua
-./luatool.py -c -f pluginloader.lua
-./luatool.py -f wificonnect.lua
-#./luatool.py -f config.lua
-./luatool.py -f config_tent.lua -t config.lua
-./luatool.py -f ledserver.lua
-./luatool.py -f wsled.lua -r
+#./luatool.py -f $PWD/config.lua
+./luatool.py -f $OLD_PWD/config_tent.lua -t config.lua
+./luatool.py -f $OLD_PWD/ledserver.lua
+./luatool.py -f $OLD_PWD/wsled.lua -r
 
 #./luatool.py -c -f telnet.lua
 # ./luatool.py -c -f drf_api.lua
